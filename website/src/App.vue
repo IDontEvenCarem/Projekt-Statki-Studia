76ce24a3f36fd52398a0ecd202ae2160924654d7
Kajetan Owczarek <flash3ch@gmail.com>
2023-12-17 13:29:42 +0100

    Initial commit

---
+++ website/src/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import BaseButton from './components/BaseButton.vue';
+import TheHeader from './components/TheHeader.vue';
+
+</script>
+
+<template>
+  <div class="main-layout">
+    <TheHeader></TheHeader>
+    <div class="content">
+      <BaseButton>
+        Rozpocznij nową grę
+      </BaseButton>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.main-layout {
+  display: grid;
+  grid-template: auto 1fr / 1fr;
+  padding: 1em;
+  box-sizing: border-box;
+  flex-direction: column;
+  min-height: 100vh;
+  gap: 1em;
+}
+</style>
---
+++ website/src/components/TheHeader.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="the-header">
+        <div class="title">
+            Gra w Statki
+        </div>
+        <div class="breadcrumbs">
+            
+        </div>
+        <div class="right">
+            <div class="user">
+                <div class="user-name">
+                    <span>Jan Kowalski</span>
+                </div>
+                <div class="user-subline">
+                    <span>1037 Pkt.</span>
+                </div>
+                <div class="user-avatar">
+                    <img src="https://picsum.photos/50/50" alt="avatar">
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.the-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0.5em 1em;
+    background-color: var(--main-color);
+    color: #fff;
+    box-sizing: border-box;
+    border-radius: var(--border-radius);
+}
+
+.title {
+    font-size: 1.5em;
+    font-weight: bold;
+}
+
+.right {
+    display: flex;
+    align-items: center;
+}
+.user {
+    display: grid;
+    align-items: center;
+    gap: 0.1em 0.5em;
+    grid-template: "name avatar" auto
+                   "subline avatar" auto / auto 50px;
+    text-align: right;
+}
+.user-name {
+    font-weight: bold;
+}
+.user-subline {
+    font-size: 0.8em;
+}
+.user-avatar {
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    overflow: hidden;
+    grid-area: avatar;
+}
+
+</style>
